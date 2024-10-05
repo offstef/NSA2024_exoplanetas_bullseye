4dc87328a7ae671e118a38531e750b15a1a059e8
stef <munteanuse@proton.me>
2024-10-05 10:59:25 +0200

    Update exoplanet-sphere.component.ts

--- src/app/components/exoplanet-sphere/exoplanet-sphere.component.ts
+++ src/app/components/exoplanet-sphere/exoplanet-sphere.component.ts
@@ -75,38 +75,7 @@ export class ExoplanetSphereComponent implements OnInit, AfterViewInit {
     const material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.5 });
     this.pointCloud = new THREE.Points(geometry, material);
     this.scene.add(this.pointCloud);
-
-    // Crear líneas entre los exoplanetas más cercanos
-    this.createConnections(positions);
   }
-
-  private createConnections(positions: THREE.Vector3[]): void {
-    const lineMaterial = new THREE.LineBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.8 }); // Color rojo brillante
-    const thresholdDistance = 1.0; // Distancia máxima para conectar dos puntos
-    const linesGeometry = new THREE.BufferGeometry();
-
-    const lineVertices: number[] = [];
-
-    // Comparar cada par de puntos para ver si están dentro de la distancia
-    for (let i = 0; i < positions.length; i++) {
-      for (let j = i + 1; j < positions.length; j++) {
-        const distance = positions[i].distanceTo(positions[j]);
-        if (distance < thresholdDistance) {
-          lineVertices.push(positions[i].x, positions[i].y, positions[i].z);
-          lineVertices.push(positions[j].x, positions[j].y, positions[j].z);
-        }
-      }
-    }
-
-    // Solo agregar las líneas si hay puntos que conectan
-    if (lineVertices.length > 0) {
-      linesGeometry.setAttribute('position', new THREE.Float32BufferAttribute(lineVertices, 3));
-      const lines = new THREE.LineSegments(linesGeometry, lineMaterial);
-      this.scene.add(lines);
-    }
-  }
-
-
   private moveCameraToExoplanet(exoplanet: ExoplanetData): void {
     const raInRadians = THREE.MathUtils.degToRad(exoplanet.ra);
     const decInRadians = THREE.MathUtils.degToRad(exoplanet.dec);
