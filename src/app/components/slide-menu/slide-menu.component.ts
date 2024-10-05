a5772d897b4243239ac046592b2af083ffbadbf8
Tron89 <126179079+Tron89@users.noreply.github.com>
2024-10-05 09:03:19 +0200

    Fix card

--- src/app/components/slide-menu/slide-menu.component.ts
+++ src/app/components/slide-menu/slide-menu.component.ts
@@ -1,8 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ExoclickService } from '../../servicios/exoclick.service';
 
-declare var bootstrap: any;
-
 @Component({
   selector: 'app-slide-menu',
   templateUrl: './slide-menu.component.html',
@@ -23,14 +21,19 @@ export class SlideMenuComponent {
   }
   
   showMenu: boolean = false;
-  @ViewChild('modal', { static: false }) modal!: ElementRef;
+  @ViewChild('modal', { static: true }) modal!: ElementRef;
+  
+  closeModal(){
+    this.showMenu = false;
+  }
 
   openModal(exoplanet: any) {
-    const modalElement = this.modal.nativeElement;
+    /*const modalElement = this.modal.nativeElement;
     const modalBootstrap = new bootstrap.Modal(modalElement);
-    modalBootstrap.show();
+    modalBootstrap.show();*/
     this.exoplanetID = exoplanet.kepid;
     this.exoplanetName = exoplanet.kepler_name;
     this.exosplanetSmass = exoplanet.koi_smass;
+    this.showMenu = !this.showMenu;
   }
 }
